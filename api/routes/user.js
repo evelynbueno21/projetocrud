1043552b8f3ec0c507b1d78ce175544c61014d5e
evelynbueno21 <evelynbueno628@gmail.com>
2024-05-09 20:31:45 -0300

    projeto full stack

---
+++ api/routes/user.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getUsers, addUser, updateUser, deleteUser } from "../controllers/user.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.post("/", addUser);
+
+router.put("/", updateUser);
+
+router.delete("/", deleteUser);
+
+export default router; 
