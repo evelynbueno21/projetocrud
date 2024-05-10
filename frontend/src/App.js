2f01df454d2ae16c56838485805d6ddfd4502440
evelynbueno21 <167826118+evelynbueno21@users.noreply.github.com>
2024-05-09 23:51:22 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import GlobalStyle from "./styles/global";
+import GlobalStyle from "../src/style/global.js";
 import styled from "styled-components";
 import Form from "./components/Form.js";
 import Grid from "./components/Grid";
